7326073b79a586517bb8f77b9d0e68a1f8b679fc
SonGlad <okoshevy@gmail.com>
2023-06-04 17:59:59 +0300

    summary

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
+import './sass/_common.scss'
+// import './css/style.css'
 import SlimSelect from 'slim-select';
 import Notiflix from 'notiflix';
+// import 'slim-select/dist/slimselect.css';
 
 
 
 
-new SlimSelect({
-  select: '#selectElement'
-});npm
+// new SlimSelect({
+//   select: '#selectElement'
+// });npm
 
+
+const refs = {
+    selectEl: document.querySelector(".breed-select"),
+    loadingMsgEl: document.querySelector(".loader"),
+    errorMsgEl: document.querySelector(".error"),
+    divEl: document.querySelector(".cat-info"),
+};
 
